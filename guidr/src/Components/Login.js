5dbac2de64117059dfa19fd16c413d1cafdc1cb8
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-21 12:21:47 -0700

    refactored login page to not use withFormik

--- guidr/src/Components/Login.js
+++ guidr/src/Components/Login.js
@@ -1,41 +1,46 @@
 import React from "react";
-import { withFormik, Form, Field } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const Login = ({ values, touched, errors, status }) => {
+const Login = props => {
+  const { history } = props;
   // Returns the form component and the error messages when the input field is touched and left empty, or just left empty
   return (
     <div className="container">
       <h1>Log In</h1>
-      <Form className="form-control">
-        <Field type="email" name="email" placeholder="E-mail" />
-        {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
+      <Formik
+        className="form-control"
+        initialValues={{ email: "", password: "" }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string().required("Please enter user's email"),
+          password: Yup.string().required("Please enter user's password")
+        })}
+        onSubmit={values => {
+          console.log(values);
+          history.push("/profile");
+        }}
+        render={({ errors, status, touched }) => (
+          <Form>
+            <Field type="email" name="email" placeholder="E-mail" />
+            {touched.email && errors.email && (
+              <p className="error">{errors.email}</p>
+            )}
 
-        <Field type="password" name="password" placeholder="Enter Password" />
-        {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
+            <Field
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+            {touched.password && errors.password && (
+              <p className="error">{errors.password}</p>
+            )}
 
-        <button type="submit">Log In</button>
-      </Form>
+            <button type="submit">Log In</button>
+          </Form>
+        )}
+      />
     </div>
   );
 };
 
-const FormikFormComponent = withFormik({
-  mapPropsToValues({ email, password }) {
-    return {
-      email: email || "",
-      password: password || ""
-    };
-  },
-  // Tests if the errors exist
-  validationSchema: Yup.object().shape({
-    email: Yup.string().required("Please enter user's email"),
-    password: Yup.string().required("Please enter user's password")
-  })
-})(Login);
-
-export default FormikFormComponent;
+export default Login;
