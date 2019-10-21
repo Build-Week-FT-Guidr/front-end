07acab83bf3afd26a1162cd9ada085156ee5edef
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-21 11:25:34 -0700

    Added Log In form and Yup validation

--- guidr/src/Components/Login.js
+++ guidr/src/Components/Login.js
@@ -1,13 +1,41 @@
 import React from "react";
-import Formik from "formik";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const Login = () => {
+const Login = ({ values, touched, errors, status }) => {
+  // Returns the form component and the error messages when the input field is touched and left empty, or just left empty
   return (
-    <div>
-      <h1>Log in</h1>
+    <div className="container">
+      <h1>Log In</h1>
+      <Form className="form-control">
+        <Field type="email" name="email" placeholder="E-mail" />
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+
+        <Field type="password" name="password" placeholder="Enter Password" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+
+        <button type="submit">Log In</button>
+      </Form>
     </div>
   );
 };
 
-export default Login;
+const FormikFormComponent = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  // Tests if the errors exist
+  validationSchema: Yup.object().shape({
+    email: Yup.string().required("Please enter user's email"),
+    password: Yup.string().required("Please enter user's password")
+  })
+})(Login);
+
+export default FormikFormComponent;
