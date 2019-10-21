cf53d215173596c55ac18c1790b2e52ec4b72d19
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-21 16:50:01 -0500

    adding to merge

--- guidr/src/Components/TripCard.js
+++ guidr/src/Components/TripCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TripCard = props => {
-    const {title, description, distance, date} = props.trip;
+    const {title, description, distance, date} = props.dummyTrips;
 
     return(
         <h4></h4>
