4beedf9b1e60a6a0e3384dc2fb7521686221c0a2
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-21 16:10:30 -0400

    adds scss folder and imports

--- guidr/src/App.js
+++ guidr/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
--- guidr/src/Components/Navigation/NavigationLinks.js
+++ guidr/src/Components/Navigation/NavigationLinks.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavigationLinks = () => {
   return (
-    <div>
+    <div className='navigation-links'>
       <Link to="">Home</Link>
       <Link to="login">Login</Link>
       <Link to="signup">Sign Up</Link>
