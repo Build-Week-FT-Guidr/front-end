c0e70b7e78aad30399cc557085ced6cdcee49e19
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-22 14:12:17 -0700

    Updated styling and added New Trip form to be filled out. Working on Apping in formik with Yup validation

--- guidr/src/Components/NewTrip.js
+++ guidr/src/Components/NewTrip.js
@@ -8,9 +8,30 @@ const NewTrip = props => {
   return (
     <section>
       <h1>Add A Trip</h1>
-      <form>
-        <input type="text" placeholder="Trip Title" />
-        <textarea type="text" placeholder="Trip description..."></textarea>
+      <form className="newtrip-form-container">
+        <label for="tripDate">Trip Date</label>
+        <input name="tripDate" type="date" />
+        <input name="tripTitle" type="text" placeholder="Trip Title..." />
+        <textarea
+          name="tripDescription"
+          type="text"
+          placeholder="Trip description..."
+        ></textarea>
+        <input
+          name="tripDuration"
+          type="text"
+          placeholder="Trip Duration (1 day, 2 weeks, etc)..."
+        />
+        <input
+          name="tripDistance"
+          type="text"
+          placeholder="Trip Distance (3 miles)..."
+        />
+        <input
+          name="tripType"
+          type="text"
+          placeholder="Excursion, cruise, hike, boating, etc..."
+        />
         <button type="submit">Add Trip</button>
       </form>
     </section>
