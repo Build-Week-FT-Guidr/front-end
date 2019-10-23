2d8c3668017f3a822276f79b0853b20dee29f7b9
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-23 10:16:19 -0700

    Styled login and signup form, fixed validation on signup form for username, and styled profile components

--- guidr/src/Components/Signup.js
+++ guidr/src/Components/Signup.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
-import axiosWithAuth from '../utils/axiosWithAuth';
+import axiosWithAuth from "../utils/axiosWithAuth";
 // axiosWithAuth
 
 const Signup = props => {
@@ -13,26 +13,26 @@ const Signup = props => {
         initialValues={{
           name: "",
           username: "",
-          password: "",
+          password: ""
           // confirmPassword: ""
         }}
         validationSchema={Yup.object().shape({
           name: Yup.string().required("Please enter your first and last name"),
-          username: Yup.string().required("Please enter your email"), // check for @
-          password: Yup.string().required("Please enter a password"),
-        //   confirmPassword: Yup.string()
-        //     .required("Please confirm your password")
-        //     .oneOf([Yup.ref("password"), null], "Passwords must match") //check that passwords match
-         })}
+          username: Yup.string().required("Please enter your username"), // check for @
+          password: Yup.string().required("Please enter a password")
+          //   confirmPassword: Yup.string()
+          //     .required("Please confirm your password")
+          //     .oneOf([Yup.ref("password"), null], "Passwords must match") //check that passwords match
+        })}
         onSubmit={values => {
           console.log(values);
           axiosWithAuth()
             .post(`/users/register`, values)
             .then(res => {
-              console.log(res)
-              history.push("/profile")
+              console.log(res);
+              history.push("/profile");
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
         }}
         render={({ errors, status, touched }) => (
           <Form className="signup-form">
@@ -40,7 +40,7 @@ const Signup = props => {
             {touched.name && errors.name && <p>{errors.name}</p>}
 
             <Field type="text" name="username" placeholder="Username" />
-            {touched.email && errors.email && <p>{errors.email}</p>}
+            {touched.username && errors.username && <p>{errors.username}</p>}
 
             <Field type="password" name="password" placeholder="password" />
             {touched.password && errors.password && <p>{errors.password}</p>}
