fcfff6d93fa8e8687304316c7924f03afe7a00d2
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-23 16:21:58 -0400

    adds contexts

--- guidr/src/App.js
+++ guidr/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { Route } from "react-router-dom";
 import axiosWithAuth from './utils/axiosWithAuth';
 
@@ -14,14 +14,31 @@ import TripCard from "./Components/TripCard";
 import NewTrip from "./Components/NewTrip";
 import Browse from './Components/Browse'
 
+import UserContext from './contexts/UserContext';
+import UsersTripsContext from './contexts/UsersTripsContext';
+
 
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [userTrips, setUserTrips] = useState([]);
+
   useEffect(() => {
     axiosWithAuth()
     .get('/users')
     .then(res => {
       console.log(res)
+      setUsers(res.data)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get('/users/1/trips')
+    .then(res => {
+      console.log(res)
+      setUserTrips(res.data)
     })
     .catch(err => console.log(err))
   }, [])
@@ -29,13 +46,18 @@ function App() {
   return (
     <div className="App">
       <NavigationLinks />
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/trip" component={TripCard} />
-      <Route path="/newtrip" component={NewTrip} />
-      <Route path='/browse' component={Browse} />
+      <UserContext.Provider value={users}>
+        <UsersTripsContext.Provider value={userTrips}>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/trip" component={TripCard} />
+        <Route path="/newtrip" component={NewTrip} />
+        <Route path='/browse' component={Browse} />
+        </UsersTripsContext.Provider>
+      </UserContext.Provider>
+      
       <Footer />
     </div>
   );
