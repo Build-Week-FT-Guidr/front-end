5b2ed3a9ec7103f21d8088dee80c24eb5e7a9f51
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-24 12:07:05 -0500

    post request to add new items is working

--- guidr/src/Components/NewTrip.js
+++ guidr/src/Components/NewTrip.js
@@ -1,79 +1,102 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Formik, Form, Field, Text } from "formik";
 import * as Yup from "yup";
 import axiosWithAuth from "../utils/axiosWithAuth";
+import UsersContext from "../contexts/UserContext";
 
 const NewTrip = props => {
   const { history } = props;
+  const users = useContext(UsersContext)
+  console.log(users)
   // Returns the form component and the error messages when the input field is touched and left empty, or just left empty
   return (
     <section>
       <h1>Add A Trip</h1>
       <Formik
         initialValues={{
-          tripDate: "",
-          tripTitle: "",
-          tripDescription: "",
-          tripDuration: "",
-          tripDistance: "",
+          title: '',
+          image: "img",
+          description: "",
+          isPrivate: false,
+          isProfessional: false,
+          duration: "",
+          distance: "",
+          date: '',
           tripType: ""
         }}
         validationSchema={Yup.object().shape({
-          tripDate: Yup.string().required("Please enter your trip start date"),
-          tripTitle: Yup.string().required("Please enter your trip's title"),
-          tripDescription: Yup.string().required(
+          date: Yup.string().required("Please enter your trip start date"),
+          title: Yup.string().required("Please enter your trip's title"),
+          description: Yup.string().required(
             "Please enter details about your trip"
           ),
-          tripDuration: Yup.string().required(
+          duration: Yup.string().required(
             "Please enter the duration of your trip"
           ),
-          tripDistance: Yup.string().required(
+          distance: Yup.string().required(
             "Please enter the distance of your trip"
           ),
           tripType: Yup.string().required("Please enter type of your trip")
         })}
         onSubmit={values => {
           console.log(values)
-          // axiosWithAuth()
+          axiosWithAuth()
+          .post(`users/10/trips`, {
+            title: values.title,
+            image: "img",
+            description: values.description,
+            isPrivate: false,
+            isProfessional: false,
+            duration: values.duration,
+            distance: values.distance,
+            date: values.date,
+            tripType: values.tripType
+          })
+          .then(res => {
+            console.log(res)
+          })
+          .catch(err => {
+            console.log('no new trip', err)
+          })
 
         }}
         render={({ errors, status, touched }) => (
           <Form className="newtrip-form-container">
-            <label htmlFor="tripDate">Trip Date</label>
-            <Field name="tripDate" type="date" />
-            {touched.tripDate && errors.tripDate && <p>{errors.tripDate}</p>}
+            <label htmlFor="date">Trip Date</label>
+            <Field name="date" type="date" />
+            {touched.date && errors.date && <p>{errors.date}</p>}
 
-            <Field name="tripTitle" type="text" placeholder="Trip Title..." />
-            {touched.tripTitle && errors.tripTitle && (
-              <p className="error">{errors.tripTitle}</p>
+            <Field name="title" type="text" placeholder="Trip Title..." />
+            {touched.title && errors.title && (
+              <p className="error">{errors.title}</p>
             )}
 
             <Field
-              name="tripDescription"
+              name="description"
               type="text"
               placeholder="Trip description..."
               component="textarea"
             />
-            {touched.tripDescription && errors.tripDescription && (
-              <p className="error">{errors.tripDescription}</p>
+            {touched.description && errors.description && (
+              <p className="error">{errors.description}</p>
             )}
 
             <Field
-              name="tripDuration"
+              name="duration"
               type="text"
               placeholder="Trip Duration (1 day, 2 weeks, etc)..."
             />
-            {touched.tripDuration && errors.tripDuration && (
-              <p className="error">{errors.tripDuration}</p>
+            {touched.duration && errors.duration && (
+              <p className="error">{errors.duration}</p>
             )}
 
             <Field
-              name="tripDistance"
+              name="distance"
               type="text"
               placeholder="Trip Distance (3 miles)..."
             />
-            {touched.tripDistance && errors.tripDistance && (
-              <p className="error">{errors.tripDistance}</p>
+            {touched.distance && errors.distance && (
+              <p className="error">{errors.distance}</p>
             )}
 
             <Field
