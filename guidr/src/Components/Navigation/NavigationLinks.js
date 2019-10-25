b8bad41f525115cc64d099b9ca794aac94aa4ea3
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-25 08:33:56 -0700

    Fixed routes in navigation page

--- guidr/src/Components/Navigation/NavigationLinks.js
+++ guidr/src/Components/Navigation/NavigationLinks.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavigationLinks = (props) => {
-  const id = localStorage.getItem('id')
+const NavigationLinks = props => {
+  const id = localStorage.getItem("id");
 
   const logOut = () => {
     if (id) {
-      if (window.confirm('Are you sure you want to log out?')) {
-        props.history.push('/');
-       localStorage.clear();
-     }
-  } 
-}
-  
+      if (window.confirm("Are you sure you want to log out?")) {
+        props.history.push("/");
+        localStorage.clear();
+      }
+    }
+  };
+
   return (
     <div className="navigation-links">
       <h1>
@@ -20,10 +20,10 @@ const NavigationLinks = (props) => {
       </h1>
       <div className="links-container">
         <Link to="">Home</Link>
-        <Link to='browse'>Browse</Link>
-        <Link to="login">Login</Link>
-        <Link to="signup">Sign Up</Link>
-        <Link to={`profile/${id}`}>My Profile</Link>
+        <Link to="/browse">Browse</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to={`/profile/${id}`}>My Profile</Link>
         <Link onClick={logOut}>Log Out</Link>
       </div>
     </div>
